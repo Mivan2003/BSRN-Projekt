1737a2548f2e94f2705d03bedaf8e9d08b5c62aa
luigi <luigi.da.silva@hotmail.com>
2024-05-02 13:08:33 +0200

    Habe diese 2 Punkte gemacht: • Der Bereitstellung des Ressourcenvektors soll interaktiv durch Eingabe möglich sein. • Zudem soll es möglich sein, einen Ressourcenvektors aus einer lokalen Datei beim Start des Programms einzulesen. Pfad und Dateiname der Eingabedatei definieren die Benutzer per Kommandozeilenargument, also z.B. -ressourcenvektor <dateiname>.

--- pythonProject/Main.py
+++ pythonProject/Main.py
@@ -1,8 +1,20 @@
-print("Halloo")
-print("hi")# todo
-a = 2
-b = 35
-if a < b:
-    print("a")
-c = 3
+print("Wollen Sie den Resscourcenvektor selbst angeben oder aus einer Datei zugreifen? ")
 
+while True:
+    #Abfrage ob man den Existing Rescource Vektor selbst eingibt oder durch eine Datei zugreift
+    eingabe = input('Tippen Sie "d" ein für die Datein oder "s", wenn Sie es selbst eingeben wollen: ')
+    if eingabe == "s":
+        print("Eingabe ist s")
+        # Erstellung des Ressourcenvektors durch eigene Eingabe
+        print("Geben Sie nun die Ressourcen ein.")
+        eResource = [int(input("Klasse 1: ")), int(input("Klasse 2: ")), int(input("Klasse 3: "))]
+        print(f"Der Ressourcenvektor ist: {eResource}")
+        break
+    elif eingabe == "d":
+        print("Eingabe ist d")
+        # Erstellung des Ressourcenvektors mithilfe einer Datei
+        datei = open(input("Geben Sie den Dateinamen ein: "), "r")
+        print(f"Der Ressourcenvektor ist: {datei.read()}")
+        break
+    else:
+        print("Eingabe war falsch")
