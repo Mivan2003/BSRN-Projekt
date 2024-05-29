9c2a1055e9f3254925d6244769369809416b47b7
champagneabii <canuslu-uni@hotmail.com>
2024-05-29 15:27:03 +0200

    REQ wurde hinzugefügt.

--- pythonProject/Main.py
+++ pythonProject/Main.py
@@ -4,7 +4,7 @@ import random
 print("Wollen Sie den Resscourcenvektor selbst angeben oder aus einer Datei zugreifen? ")
 
 while True:
-    # Abfrage ob man den Existing Rescource Vektor selbst eingibt oder durch eine Datei zugreift
+    # Abfrage, ob man den Existing Rescource Vektor selbst eingibt oder durch eine Datei zugreift
     eingabe = input('Tippen Sie "d" ein für die Datein oder "s", wenn Sie es selbst eingeben wollen: ')
     if eingabe == "s":
         print("Eingabe ist s")
@@ -59,6 +59,9 @@ def matrix_erstellung(matrix):
         if matrix == "Belegungsmatrix":
             wahl1 = int(input(
                 'Wollen Sie die Belegungsmatrix selbst eingeben "1" oder die, im vorhinein festgelegte Matrix benutzten "2"? '))
+        elif matrix == "Anforderungsmatrix":
+            wahl1 = int(input(
+                'Wollen Sie die Anforderungsmatrix selbst eingeben "1" oder eine zufällige Matrix generieren lassen "2"? '))
         if wahl1 == 1:
             print("Die Matrix besteht aus 3 Prozessen und 3 Klassen. "
                   "Bitte geben Sie nun die Matrix an:")
@@ -72,6 +75,9 @@ def matrix_erstellung(matrix):
                  int(input("Prozess 3, Klasse 3: "))]]
             if matrix == "Belegungsmatrix":
                 print(f"\nDie Belegungsmatrix ist: \n{matrix1[0]}\n{matrix1[1]}\n{matrix1[2]}\n")
+            elif matrix == "Anforderungsmatrix":
+                print(f"\nDie Anforderungsmatrix ist: \n{matrix1[0]}\n{matrix1[1]}\n{matrix1[2]}\n")
+            return matrix1
         elif wahl1 == 2:
             if matrix == "Belegungsmatrix":
                 matrix = [[1, 0, 1],
@@ -80,6 +86,163 @@ def matrix_erstellung(matrix):
                 print(f"\nDie Belegungsmatrix ist: \n{matrix[0]}\n{matrix[1]}\n{matrix[2]}\n")
                 return matrix
 
+            elif matrix == "Anforderungsmatrix":
+                rows = 3
+                cols = 3
+                max_values_per_row = [eResource[0], eResource[1], eResource[2]]  # Maximalwerte pro Zeile
+                matrix = [[random.randint(0, max_values_per_row[row]) for _ in range(cols)] for row in range(rows)]
+                # Ausgabe der Anforderungsmatrix
+                print(f"\nDie Anforderungsmatrix ist: \n{matrix[0]}\n{matrix[1]}\n{matrix[2]}\n")
+                return matrix
+        else:
+            print("Die Eingabe war falsch! Bitte Option '1' oder '2' wählen!\n")
+
 
 # Erstellung der Belegungsmatrix
 alloc = matrix_erstellung("Belegungsmatrix")
+
+
+def ressourcen_belegung(prozess, matrix):
+    beListe = matrix[prozess - 1]
+    if len(klasse1) >= beListe[0]:  # Prüft, ob es überhaupt noch verfügbare Ressourcen gibt
+        for i in range(beListe[0]):
+            r = klasse1.pop(0)  # zeigt und entfernt die erste ressource
+            belegt1[0][prozess - 1].append(r)  # Macht die belegten Ressourcen zur Übersicht in eine Liste
+            # print(f"Prozess {prozess} belegt Ressource der Klasse 1 ")
+    else:
+        print(f"Prozess {prozess} kann die Ressource der Klasse 1 nicht belegen!")  # Ressource ist nicht verfügbar
+
+    if len(klasse2) >= beListe[1]:
+        for i in range(beListe[1]):
+            r = klasse2.pop(0)  # zeigt und entfernt die zweite ressource
+            belegt1[1][prozess - 1].append(r)
+            # print(f"Prozess {prozess} belegt Ressource der Klasse 2 ")
+    else:
+        print(f"Prozess {prozess} kann die Ressource der Klasse 2 nicht belegen!")
+
+    if len(klasse3) >= beListe[2]:
+        for i in range(beListe[2]):
+            r = klasse3.pop(0)  # zeigt und entfernt die dritte ressource
+            belegt1[2][prozess - 1].append(r)
+            # print(f"Prozess {prozess} belegt Ressource der Klasse 3 ")
+    else:
+        print(f"Prozess {prozess} kann die Ressource der Klasse 3 nicht belegen!")
+
+
+# Einfach nur fürs Design
+print("Ressourcenrestvektor wird berechnet... ")
+
+time.sleep(2)
+
+# Überprüft, ob ein Prozess schon einmal komplett durchlaufen ist
+wenn_ausgefuehrt = False
+wenn_ausgefuehrt2 = False
+wenn_ausgefuehrt3 = False
+
+
+# Überprüfung, ob ein Deadlock vorhanden ist
+def pruefe_deadlock(prozess):
+    global wenn_ausgefuehrt
+    global wenn_ausgefuehrt2
+    global wenn_ausgefuehrt3
+
+    beListe = req[prozess - 1]
+    # Prüft, ein Prozess durchgeführt werden kann
+    if len(klasse1) >= beListe[0] and len(klasse2) >= beListe[1] and len(klasse3) >= beListe[2]:
+        if not wenn_ausgefuehrt and prozess == 1:
+            return False
+        if not wenn_ausgefuehrt2 and prozess == 2:
+            return False
+        if not wenn_ausgefuehrt3 and prozess == 3:
+            return False
+    else:
+        return True
+
+
+# Nachdem ein Programm durchlaufen ist, werden die Ressourcen freigegeben
+def freigabe(prozess):
+    for i in range(len(belegt1[0][prozess - 1])):
+        re = belegt1[0][prozess - 1].pop(0)
+        klasse1.append(re)
+    for i in range(len(belegt1[1][prozess - 1])):
+        re = belegt1[1][prozess - 1].pop(0)
+        klasse2.append(re)
+    for i in range(len(belegt1[2][prozess - 1])):
+        re = belegt1[2][prozess - 1].pop(0)
+        klasse3.append(re)
+
+# Die Ressourcen der Belegungsmatrix werden belegt
+ressourcen_belegung(1, alloc)
+ressourcen_belegung(2, alloc)
+ressourcen_belegung(3, alloc)
+
+# print(belegt1) # Nur zur überprüfung- wird später nicht benötigt
+
+# rRestVektor = [klasse1, klasse2, klasse3] # Nur zur überprüfung- wird später nicht benötigt
+rRessource = [len(klasse1), len(klasse2), len(klasse3)]  # Ressourcenrestvektor
+
+# Der erste Ressourcenrestvektor nach der Belegungsmatrix
+print(f"\nDer Ressourcenrestvektor ist: {rRessource}\n")  # Ausgabe des Ressourcenrestvektors
+
+# Erstellung der Anforderungsmatrix
+req = matrix_erstellung("Anforderungsmatrix")
+
+# Die Deadlock Erkennung
+while True:
+    # Überprüft, ob bereits am Anfang ein Deadlock entstanden ist
+    if pruefe_deadlock(1) and pruefe_deadlock(2) and pruefe_deadlock(3):
+        print("Ein Deadlock ist entstanden")
+        break
+    else:
+        # Wenn ein Prozess bereits vollendet wurde, wird es nicht mehr angezeigt
+        option = None
+        if not pruefe_deadlock(1):
+            if not wenn_ausgefuehrt:
+                print(f"Prozess {1} kann durchgeführt werden!")
+                option = 1
+        if not pruefe_deadlock(2):
+            if not wenn_ausgefuehrt2:
+                print(f"Prozess {2} kann durchgeführt werden!")
+                option = 2
+        if not pruefe_deadlock(3):
+            if not wenn_ausgefuehrt3:
+                print(f"Prozess {3} kann durchgeführt werden!")
+                option = 3
+        time.sleep(1)
+        if option is None:
+            print("\nEs ist ein Deadlock entstanden!")
+            break
+        if eingabe == "s":
+            abfrage = int(input('\nWelcher Prozess soll durchgeführt werden? '
+                                '\nWenn das Programm automatisch laufen soll, dann tippen sie "0" ein. '))
+            if not abfrage == 0:
+                ressourcen_belegung(abfrage, req)
+                freigabe(abfrage)
+                if abfrage == 1:
+                    wenn_ausgefuehrt = True
+                elif abfrage == 2:
+                    wenn_ausgefuehrt2 = True
+                elif abfrage == 3:
+                    wenn_ausgefuehrt3 = True
+            elif abfrage == 0:
+                ressourcen_belegung(option, req)
+                freigabe(option)
+                if option == 1:
+                    wenn_ausgefuehrt = True
+                elif option == 2:
+                    wenn_ausgefuehrt2 = True
+                elif option == 3:
+                    wenn_ausgefuehrt3 = True
+                eingabe = "d"
+            else:
+                "Falsche Eingabe!"
+
+        elif eingabe == "d":
+            ressourcen_belegung(option, req)
+            freigabe(option)
+            if option == 1:
+                wenn_ausgefuehrt = True
+            elif option == 2:
+                wenn_ausgefuehrt2 = True
+            elif option == 3:
+                wenn_ausgefuehrt3 = True
